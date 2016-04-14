c6ff3b6ff947060ebb25a4b6bc023feed61b54cc
Alexander Lombardo <alexanderlombardo@Alexanders-MacBook-Pro.local>
2016-04-14 09:02:44 -0700

    Clears edit text field on button push

--- ToDoProject1/app/src/main/java/com/example/alexanderlombardo/todoproject1/MainActivity.java
+++ ToDoProject1/app/src/main/java/com/example/alexanderlombardo/todoproject1/MainActivity.java
@@ -1,43 +1,59 @@
 package com.example.alexanderlombardo.todoproject1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> getNewList;
-    ArrayAdapter<String> mAdapter;
-    ListView lvItems;
-    Button listbutton;
+    ArrayList<String> arrayListToDo;
+    ArrayAdapter<String> arrayAdapterToDo;
+    public ListView listView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+//      **NOT REALLY SURE WHAT THIS IS DOING YET....  Building the Array List?**
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getNewList.add("Testing List");
 
+        arrayListToDo = new ArrayList<String>();
+        arrayAdapterToDo = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayListToDo);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapterToDo);
+
+ //       listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+   //     });
+    }
+
+//      **ADDS A NEW ENTRY IN EDIT TEXT FIELD TO MAIN LIST WHEN BUTTON CLICKED**
+
+    public void listAddPage(View v) {
+
+        String listAddList = ((EditText)findViewById(R.id.editText)).getText().toString().trim();
+
+        if(listAddList.isEmpty()){
+            return;
+        }
+        arrayAdapterToDo.add(listAddList);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        editText.setText("");
     }
 
 
 
-    public void listAdd(View v) {
-        EditText getNewList = (EditText) findViewById(R.id.getNewList);
-        String itemText = getNewList.getText().toString();
-        mAdapter.add(itemText);
-        getNewList.setText("");
-    }
-
-//    public void onAddItem(View v) {
-//        EditText getNewItem = (EditText) findViewById(R.id.getNewItem);
-//        String itemText = etNewItem.getText().toString();
-//        itemsAdapter.add(itemText);
-//        getNewItem.setText("");
+    //**SET LONG CLICK TO DELETE ITEMS HERE**
+//    public void itemAddList(View v) {
+//
+//        arrayAdapterToDo.notifyDataSetChanged();
 //    }
+
+//      **INTENT TO MOVE TO ITEM LIST**
+//        Intent intent = new Intent(MainActivity.this, todolistitem.class);
 
 
 }
---
+++ ToDoProject1/app/src/main/java/com/example/alexanderlombardo/todoproject1/todolistitem.java
@@ -0,0 +1,7 @@
+package com.example.alexanderlombardo.todoproject1;
+
+/**
+ * Created by alexanderlombardo on 4/13/16.
+ */
+public class todolistitem {
+}
